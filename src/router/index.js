908bf2fd9a16abdedcc775a5cc5cf9cbfd52dec4
reskuy <48839288+reskuy@users.noreply.github.com>
2020-09-14 14:10:59 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
-import vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from "@/components/Login"
 import About from "@/components/About"
 import Log from "@/components/Log"
 import Register from "@/components/Register"
+import User from '@/components/User'
+import EditUser from '@/components/EditUser'
 
-Vue.use(VueRouter)
 
-const router = new VueRouter({
+Vue.use(VueRouter);
+
+//const router = new VueRouter({
+  export default new VueRouter({
     routes : [
-      {path:'/',name:'Login',component: Login},
+      {path:'/',name:'User',component: User},
+      {path:'/Index',name:'Index',component: User},
       {path:'/About',name:'About',component: About},
       {path:'/Log',name:'Log',component: Log},
-      {path:'/Register',name:'Register',component: Register}
+      {path:'/Register',name:'Register',component: Register},
+      {path:'/User',name:'User',component: User},
+      {path:'/Log/:id',name:'EditUser',component: EditUser}
     ],
     mode: 'history'
   });
+
+  
