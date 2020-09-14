1e252630dfa923830a3658d2f968510cd1bc3cab
reskuy <48839288+reskuy@users.noreply.github.com>
2020-09-14 14:11:41 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="black"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="assets/logo.svg"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <router-link class="routerlink" to="/"><v-btn text>
-        <span class="mr-2">Home</span>
-        <v-icon>mdi-home</v-icon>
-      </v-btn></router-link>
-
-
-      <router-link class="routerlink" to="About"><v-btn text>
-        <span class="mr-2">About</span>
-        <v-icon>mdi-alert-circle</v-icon>
-      </v-btn></router-link>
-
-      <v-btn
-        href="http://trinityauto.id/#Services"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Service</span>
-        <v-icon>mdi-cog-outline</v-icon>
-      </v-btn>
-      <v-btn
-        href="http://trinityauto.id/#Clients"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Client</span>
-        <v-icon>mdi-account-heart-outline</v-icon>
-      </v-btn>
-      <v-btn
-        href="http://trinityauto.id/#Contact"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Contact</span>
-        <v-icon>mdi-contacts-outline</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+  <Navbar/>
     <v-main>
       <router-view/>
     </v-main>
@@ -74,11 +12,12 @@
 }
 </style>
 <script>
-
+import Navbar from './components/Navbar'
 export default {
   name: 'App',
 
   components: {
+    Navbar,
   },
 
   data: () => ({
