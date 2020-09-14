b5ef94338c3551000786f0d5bbac7c6d8e5059ea
reskuy <48839288+reskuy@users.noreply.github.com>
2020-09-14 14:12:10 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,27 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueRouter from 'vue-router'
-import Login from "@/components/Login"
-import About from "@/components/About"
-import Log from "@/components/Log"
-import Register from "@/components/Register"
-import User from '@/components/User'
+import router from '@/router/index'
 
-Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 
-
-const router = new VueRouter({
-    routes : [
-      {path:'/',name:'Login',component: Login},
-      {path:'/About',name:'About',component: About},
-      {path:'/Log',name:'Log',component: Log},
-      {path:'/Register',name:'Register',component: Register},
-      {path:'/User',name:'User',component: User}
-    ],
-    mode: 'history'
-  });
 
 new Vue({
   router,
